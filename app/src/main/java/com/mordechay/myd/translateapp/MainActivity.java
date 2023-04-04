c12c0579cacf2aba055117d668b5449bae9aeddc
mordechay <m8572998@gmail.com>
2023-04-04 03:12:24 +0300

    upgrade view and add code

--- app/src/main/java/com/mordechay/myd/translateapp/MainActivity.java
+++ app/src/main/java/com/mordechay/myd/translateapp/MainActivity.java
@@ -13,10 +13,14 @@ import android.view.View;
 import android.widget.EditText;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -27,8 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText edtFrom;
     private EditText edtTo;
     private ActivityResultLauncher<Intent> getSelectedFileActivityResult;
-    private String skippedChar[] = null;
-    String[] arr;
+    private String skippedChar = "Cellular";
+    String[][] arr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,36 +93,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Document doc = dBuilder.parse(inputStream);
                     doc.getDocumentElement().normalize();
                     NodeList nList = doc.getElementsByTagName("string");
-                    arr = new String[nList.getLength()];
+                    arr = new String[nList.getLength()][];
                     String content = "";
-                    String allContent = "";
+                    StringBuilder allContent = new StringBuilder();
+                    String[] arrName = new String[nList.getLength()];
                     for (int i = 0; i < nList.getLength(); i++) {
                         Node nNode = nList.item(i);
                         content = nNode.getTextContent().trim();
-                        allContent += nNode.getTextContent().trim();
-                        if (skippedChar != null && !skippedChar[0].isEmpty()) {
-                            for (int b = 0; b < skippedChar.length; b++) {
-                                if (content.contains(skippedChar[i])) {
-                                    arr[i] = content.split(skippedChar[i])[0];
-                                    arr[i + 1] = skippedChar[i];
-                                    arr[i + 2] = content.split(skippedChar[i])[1];
-                                    i += 2;
-                                } else {
-                                    arr[i] = content;
+                        allContent.append(nNode.getTextContent().trim()).append("\n");
+                        // חיפוש והוספת הערך של ה-attribut name למערך
+                        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element element = (Element) nNode;
+                            arrName[i] = element.getAttribute("name");
+                        }
+
+                        if (skippedChar != null && !skippedChar.isEmpty()) {
+                            String[] splittedContent = content.split(Pattern.quote(skippedChar));
+                            if (splittedContent.length > 1) {
+                                ArrayList<String> splittedContentList = new ArrayList<>();
+                                for (String s : splittedContent) {
+                                    if (!s.isEmpty()) {
+                                        splittedContentList.add(s);
+                                    }
                                 }
+                                arr[i] = splittedContentList.toArray(new String[0]);
+                            } else {
+                                arr[i] = new String[]{content};
                             }
-                        }else{
-                            arr[i] = content;
+                        } else {
+                            arr[i] = new String[]{content};
                         }
                     }
-                    String finalContent = allContent;
+
+                        String finalContent = allContent.toString();
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             edtFrom.setText(finalContent);
                             if(BuildConfig.DEBUG) {
-                                for (String s : arr) {
-                                    Log.d(this.getClass().getName(), s);
+                                for (int i = 0; i < arrName.length; i++) {
+                                    StringBuilder strBody = new StringBuilder("name=  " + arrName[i] + "  value=  ");
+                                    for (int b = 0; b < arr[i].length; b++) {
+                                       strBody.append(arr[i][b]).append("\n");
+                                    }
+                                    Log.d(this.getClass().getName(), strBody.toString());
                                 }
                             }
                         }
