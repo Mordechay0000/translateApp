3ee6e8b873d24fd8bfb2617b3dda76482d715417
mordechay <m8572998@gmail.com>
2023-04-07 01:46:46 +0300

    the app worked

--- app/src/main/java/com/mordechay/myd/translateapp/MainActivity.java
+++ app/src/main/java/com/mordechay/myd/translateapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.mordechay.myd.translateapp;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -11,28 +12,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import java.io.File;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
+import java.io.OutputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int PICKFILE_RESULT_CODE = 1;
+    private static final String FILE_TYPE = "text/xml";
+    private static final String REPLACEMENT_CHARACTER = "@@@";
     private EditText edtFrom;
     private EditText edtTo;
     private ActivityResultLauncher<Intent> getSelectedFileActivityResult;
-    private String skippedChar = "Cellular";
-    String[][] arr;
+    private ActivityResultLauncher<Intent> getSaveFileActivityResult;
+    private static String[] skippedChar = new String[]{"@","*","$"};
+    private String nameAttribute;
+    private String content;
+    private static int[] locationSkippedChar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +58,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             fileUri = result.getData().getData();
                             parserXml(fileUri);
                         }else {
+                            Toast.makeText(this, getText(R.string.no_selected_file_or_directory), Toast.LENGTH_SHORT).show();
+                        }
 
+                    }
+                });
+        getSaveFileActivityResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Uri fileUri = null;
+                        if (result.getData() != null) {
+                            fileUri = result.getData().getData();
+                            saveXmlTranslate(fileUri);
+                        }else {
+                            Toast.makeText(this, getText(R.string.no_selected_file_or_directory), Toast.LENGTH_SHORT).show();
                         }
 
                     }
@@ -65,24 +89,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btn_select_file:
-                pickFile();
+                pickFile(false);
                 break;
             case R.id.btn_translate:
-                saveXmlTranslate();
+                pickFile(true);
                 break;
         }
 
     }
 
-    public void pickFile() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("text/xml");
+    public void pickFile(boolean isSave) {
+        Intent intent = new Intent(!isSave ? Intent.ACTION_GET_CONTENT : Intent.ACTION_CREATE_DOCUMENT);
+        intent.setType(FILE_TYPE);
+        if (!isSave){
+            getSelectedFileActivityResult.launch(intent);
+        }else {
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.putExtra(Intent.EXTRA_TITLE, "strings.xml");
+            getSaveFileActivityResult.launch(intent);
+        }
 
-        getSelectedFileActivityResult.launch(intent);
     }
 
+
+
+    private static String replacementSkippedChars(String content, boolean isParsing){
+            //locationSkippedChar = findString(content, skippedChar);
+            for (int i = 0; i < skippedChar.length; i++) {
+                content = content.replace(!isParsing ? skippedChar[i] : REPLACEMENT_CHARACTER + i, !isParsing ? REPLACEMENT_CHARACTER + i : skippedChar[i]);
+            }
+        return content;
+    }
+
+    /*
+    for save location skipped char's method
+    @NonNull
+    private static int[] findString(String str, String[] arrSearchStr) {
+        int[] positions = new int[str.length()];
+        int removedChars = 0;
+        int i = 0;
+        for (String searchChar: arrSearchStr) {
+            int pos = str.indexOf(searchChar);
+            while (pos != -1) {
+                positions[i++] = pos;
+                pos = str.indexOf(searchChar, pos + 1) - removedChars;
+                removedChars += searchChar.length();
+            }
+        }
+        return Arrays.copyOf(positions, i);
+    }
+
+    */
+
+
+
+    private void saveXmlTranslate(Uri UriSave) {
+        content = edtTo.getText().toString();
+        content = replacementSkippedChars(content, true);
+        String[] arrNameAttribute = nameAttribute.split("\n");
+        String[] arrContent = content.split("\n");
+        createdTranslateXml(arrNameAttribute, arrContent, UriSave);
+    }
+
+
+
+
     private void parserXml(Uri selectedFile) {
-        edtFrom.setText("מפענח קובץ");
+        edtFrom.setText(R.string.file_parser);
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -93,63 +166,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Document doc = dBuilder.parse(inputStream);
                     doc.getDocumentElement().normalize();
                     NodeList nList = doc.getElementsByTagName("string");
-                    arr = new String[nList.getLength()][];
-                    String content = "";
-                    StringBuilder allContent = new StringBuilder();
-                    String[] arrName = new String[nList.getLength()];
+
+                    nameAttribute = "";
+                    content = "";
                     for (int i = 0; i < nList.getLength(); i++) {
                         Node nNode = nList.item(i);
-                        content = nNode.getTextContent().trim();
-                        allContent.append(nNode.getTextContent().trim()).append("\n");
                         // חיפוש והוספת הערך של ה-attribut name למערך
                         if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                             Element element = (Element) nNode;
-                            arrName[i] = element.getAttribute("name");
+                            nameAttribute = nameAttribute + element.getAttribute("name") + "\n";
                         }
 
-                        if (skippedChar != null && !skippedChar.isEmpty()) {
-                            String[] splittedContent = content.split(Pattern.quote(skippedChar));
-                            if (splittedContent.length > 1) {
-                                ArrayList<String> splittedContentList = new ArrayList<>();
-                                for (String s : splittedContent) {
-                                    if (!s.isEmpty()) {
-                                        splittedContentList.add(s);
-                                    }
-                                }
-                                arr[i] = splittedContentList.toArray(new String[0]);
-                            } else {
-                                arr[i] = new String[]{content};
-                            }
-                        } else {
-                            arr[i] = new String[]{content};
-                        }
+                        content = content + nNode.getTextContent().trim() + "\n";
                     }
 
-                        String finalContent = allContent.toString();
+                    if (skippedChar != null && !skippedChar[0].isEmpty()) {
+                        content = replacementSkippedChars(content, false);
+                    }
+
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            edtFrom.setText(finalContent);
+                            edtFrom.setText(content);
                             if(BuildConfig.DEBUG) {
+                                String[] arrName = nameAttribute.split("\n");
+                                String[] arrContent = content.split("\n");
                                 for (int i = 0; i < arrName.length; i++) {
-                                    StringBuilder strBody = new StringBuilder("name=  " + arrName[i] + "  value=  ");
-                                    for (int b = 0; b < arr[i].length; b++) {
-                                       strBody.append(arr[i][b]).append("\n");
-                                    }
-                                    Log.d(this.getClass().getName(), strBody.toString());
+                                    String strBody = "name = " + arrName[i] + "  value = " + arrContent[i];
+                                    edtTo.setText(edtTo.getText() + strBody + "\n");
                                 }
                             }
                         }
                     });
                 } catch (Exception e) {
+                    runOnUiThread(() -> {
+                                Toast.makeText(MainActivity.this, getText(R.string.error_parsing_file), Toast.LENGTH_SHORT).show();
+                            }
+                    );
                     e.printStackTrace();
                 }
             }
         }).start();
     }
 
+    private void createdTranslateXml(String[] arrNameAttribute, String[] arrContent, Uri uriSave) {
+        try {
+            // יצירת  output stream לקובץ
+            OutputStream outputStream = getContentResolver().openOutputStream(uriSave);
 
-    private void saveXmlTranslate() {
+            // יצירת דוקומנט XML חדש
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
 
+            // יצירת התגים הראשיים בקובץ
+            Element rootElement = doc.createElement("resources");
+            doc.appendChild(rootElement);
+
+            // הוספת התגים לקובץ בהתאם למערכי הקלט
+            for (int i = 0; i < arrNameAttribute.length; i++) {
+                Element element = doc.createElement("string");
+                element.setAttribute("name", arrNameAttribute[i]);
+                element.appendChild(doc.createTextNode(arrContent[i]));
+                rootElement.appendChild(element);
+            }
+
+            // כתיבת התוכן לקובץ ושמירתו באחסון החיצוני של האפליקציה
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(outputStream);
+            transformer.transform(source, result);
+            Log.d("SaveToXml", "File saved successfully: " + uriSave.getPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
